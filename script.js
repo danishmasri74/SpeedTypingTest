edcf5e04c10effb7d83594833a73e78f73478779
Muhammad Danish Bin Masri <danishmasri74@gmail.com>
2023-07-08 13:00:45 +0800

    Publish

---
+++ script.js
@@ -0,0 +1,100 @@
+const paragraphs = [
+    "One key and thirty-seven locks. That was all. And seven locks of it were rusty. Locks picked one by one, prying open secrets and barriers, leaving behind a sense of intrigue and curiosity that such intricate maneuvering implied. One key and thirty-seven locks. And the next day would be an adventure...",
+    "The boat had a hidden leak that remained unnoticed, and it would stay that way for hours. However, this would soon become problematic as the vessel sailed farther into the sea, and the tiny leak would inevitably grow into a significant issue. John had orchestrated this situation meticulously.",
+    "The dice rolled on the table, tumbling repeatedly with anticipation. It tumbled to a stop, deciding its fate. Two players anxiously hoped for a specific outcome, but the dice had already chosen its result.",
+    "I'll elaborate more at our upcoming meeting tomorrow, but I think I've stumbled upon a resolution. It's not entirely within the confines of the law, but it won't lead us to spend our lives behind bars either. Are you willing to take the gamble? Anderson questioned his partner over the phone.",
+    "In his prime, he eagerly welcomed the winds of change, seeking thrills and venturing into the unknown. Regrettably, those years have long passed, leaving him with an unquenchable desire to turn back time and rekindle the enthusiasm that accompanies new experiences. Alas, his disdain for anything that disrupts his comfort now prevails."
+];
+
+const typingText = document.querySelector(".typing-text p"),
+    inpField = document.querySelector(".wrapper .input-field"),
+    tryAgainBtn = document.querySelector(".content button"),
+    timeTag = document.querySelector(".timeLeft span b"),
+    errorTag = document.querySelector(".errors span"),
+    wpmTag = document.querySelector(".wpm span"),
+    cpmTag = document.querySelector(".cpm span");
+
+let timer,
+    maxTime = 60,
+    timeLeft = maxTime,
+    charIndex = errors = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if (characters[charIndex].classList.contains("incorrect")) {
+                    errors--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                errors++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        let wpm = Math.round(((charIndex - errors) / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+        wpmTag.innerText = wpm;
+        errorTag.innerText = errors;
+        cpmTag.innerText = charIndex - errors;
+    } else {
+        clearInterval(timer);
+        inpField.value = "";
+    }
+}
+
+
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - errors) / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetTest() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = errors = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    errorTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetTest);
